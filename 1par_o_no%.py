998bea220dc6ef0db77a82a2142343571e083a66
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-03-07 11:58:31 -0500

    Add files via upload

---
+++ 1par_o_no%.py
@@ -0,0 +1,8 @@
+
+numeroInt = int(input('Ingrese un número -> '))
+
+if numeroInt % 2 == 0:
+    print('El número',numeroInt, 'es par')
+
+else:
+    print('El número',numeroInt, 'es impar')
---
+++ 2par-o-no.py
@@ -0,0 +1,8 @@
+
+numeroInt = int(input('Ingrese un número -> '))
+
+if numeroInt / 2 == numeroInt / 2:
+    print('El número',numeroInt, 'es par')
+
+else:
+    print('El número',numeroInt, 'es impar')
